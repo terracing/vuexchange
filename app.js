7a16249a695d5d4d216a29c8e603adf0b48bf263
Omar Terrazas <terrazas.omar@gmail.com>
2021-12-05 19:39:42 -0600

    Render lists

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ new Vue({
         return {
             name: 'Bitcoin',
             img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-            changePercent: 10
+            changePercent: 10,
+            prices: [8400,5200,7460,5520,7990],
+            pricesWithDays: [
+                {day: 'Monday', value: 8400},
+                {day: 'Tuesday', value: 5200},
+                {day: 'Wednesday', value: 7460},
+                {day: 'Thursday', value: 5520},
+                {day: 'Friday', value: 7990},
+                {day: 'Saturday', value: 6385},
+                {day: 'Sunday', value: 7850}
+            ]
         }
     }
 })
