4ca1986521df1e3544df7e8b3d3372ec668642cb
Omar Terrazas <terrazas.omar@gmail.com>
2021-12-07 18:35:56 -0600

    Communication between components

--- app.js
+++ app.js
@@ -1,56 +1,79 @@
-new Vue({
-    el: '#app',
-
-    data () {
+Vue.component('CoinDetail', {
+    props: ['coin'],
+    data() {
         return {
-            name: 'Bitcoin',
-            symbol: 'BTC',
-            img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-            changePercent: -10,
-            value: 0,
-            priceBtc: 7850,
-            color: 'f4f4f4',
-            darkState: false,
-            prices: [8400,5200,7460,5520,7990],
-            pricesWithDays: [
-                {day: 'Monday', value: 8400},
-                {day: 'Tuesday', value: 5200},
-                {day: 'Wednesday', value: 7460},
-                {day: 'Thursday', value: 5520},
-                {day: 'Friday', value: 7990},
-                {day: 'Saturday', value: 6385},
-                {day: 'Sunday', value: 7850}
-            ],
-
             showPrices: false,
+            value: 0
         }
     },
-
+    methods: {
+        toggleShowPrices() {
+            this.showPrices = !this.showPrices
+        }
+    },
     computed: {
-        title () {
-            return `${this.name}- ${this.symbol}`
+        title() {
+            return `${this.coin.name}-${this.coin.symbol}`
         },
 
-        convertedValue () {
-            if(!this.value) {
+        convertedValue() {
+            if (!this.value) {
                 return 0
             }
 
-            return this.value / 7850
+            return this.value / this.coin.price
         }
     },
+    template: `
+        <div class="card col-sm-4">
+            <span class="badge position-absolute top-0 end-0 translate-middle rounded-pill" v-bind:class="coin.changePercent > 0 ? 'bg-success' : 'bg-danger'"><i class="fas" v-bind:class="coin.changePercent > 0 ? 'fa-thumbs-up' : 'fa-thumbs-down'"></i></span>
+            <img v-bind:src="coin.img" v-bind:alt="coin.name" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">{{ title }}</h5>
+                <p>show prices <a href="#" v-on:click="toggleShowPrices"><i
+                    v-bind:class="showPrices ? 'fas fa-eye-slash' : 'fas fa-eye'"></i></a></p>
+                <ul class="list-group" v-show=showPrices>
+                    <li class="list-group-item"
+                        v-bind:class="{ 'text-info': price.value == coin.price, 'text-success': price.value > coin.price, 'text-danger': price.value < coin.price }"
+                        v-for="(price,index) in coin.pricesWithDays" v-bind:key="price.day">
+                        {{ price.day }} {{ price.value }}
+                    </li>
+                </ul>
+                <footer>
+                    <form action="">
+                        <div class="col">
+                            <label for="" class="form-label">MXN to {{ coin.symbol }}</label>
+                            <input type="number" v-model="value" class="form-control">
+                            <p class="text-info text-end p-2">{{ convertedValue }}</p>
+                        </div>
+                    </form>
+                </footer>
+            </div>
+        </div>
+    `
 
-    watch: {
-        showPrices (newVal, oldVal) {
-            console.log(newVal, oldVal)
+})
+
+new Vue({
+    el: '#app',
+
+    data() {
+        return {
+            btc: {
+                name: 'Bitcoin',
+                symbol: 'BTC',
+                img: 'https://images.unsplash.com/photo-1609554496796-c345a5335ceb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+                changePercent: -10,
+                price: 7850,
+                pricesWithDays: [{ day: 'Monday', value: 8400 }, { day: 'Tuesday', value: 5200 }, { day: 'Wednesday', value: 7460 }, { day: 'Thursday', value: 5520 }, { day: 'Friday', value: 7990 }, { day: 'Saturday', value: 6385 }, { day: 'Sunday', value: 7850 }
+                ],
+            },
+            color: 'f4f4f4',
+            darkState: false
         }
     },
-
     methods: {
-        toggleShowPrices () {
-            this.showPrices = !this.showPrices
-        },
-        toggleDarkMode () {
+        toggleDarkMode() {
             this.color = this.color.split('').reverse().join('')
             this.darkState = !this.darkState
         }
