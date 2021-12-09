c2d6cd329cdfd74faeb19b263b2c1c8d532c5c91
Omar Terrazas <terrazas.omar@gmail.com>
2021-12-08 23:37:30 -0600

    Manage filters

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="container mx-auto bg-gray-200">
     <vx-header />
-    <router-view class="p-4 min-height-3/4" />
+    <router-view class="p-4" />
   </div>
 </template>
 
