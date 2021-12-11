1224cc66310956d0e20eae7646620b34720cfec5
Omar Terrazas <terrazas.omar@gmail.com>
2021-12-11 11:49:56 -0600

    Filters and sort

--- src/components/VxAssetsTable.vue
+++ src/components/VxAssetsTable.vue
@@ -3,19 +3,29 @@
     <thead>
       <tr class="border-b-2 border-gray-400">
         <th></th>
-        <th>
-          <span>Ranking</span>
+        <th :class="{ up: this.sortOrder == 1, down: this.sortOrder == -1 }">
+          <span @click="changeSortOrder" class="underline cursor-pointer"
+            >Ranking</span
+          >
         </th>
         <th>Nombre</th>
         <th>Precio</th>
         <th>Cap. de Mercado</th>
         <th>Variación 24hs</th>
-        <td class="hidden sm:block"></td>
+        <td class="hidden sm:block">
+          <input
+            type="text"
+            class="bg-gray-100 focus:outline-none border-b border-gray-400 py-2 px-4 block"
+            id="filter"
+            placeholder="search..."
+            v-model="filter"
+          />
+        </td>
       </tr>
     </thead>
     <tbody>
       <tr
-        v-for="a in assets"
+        v-for="a in filteredAssets"
         :key="a.id"
         class="border-b border-gray-300 hover:bg-gray-100 hover:bg-orange-100"
       >
@@ -61,6 +71,13 @@ export default {
 
   components: { VxButton },
 
+  data() {
+    return {
+      filter: "",
+      sortOrder: "",
+    };
+  },
+
   props: {
     assets: {
       type: Array,
@@ -68,9 +85,33 @@ export default {
     },
   },
 
+  computed: {
+    filteredAssets() {
+      const altOrder = this.sortOrder == 1 ? -1 : 1;
+
+      return this.assets
+        .filter(
+          (a) =>
+            a.symbol.toLowerCase().includes(this.filter.toLowerCase()) ||
+            a.name.toLowerCase().includes(this.filter.toLowerCase())
+        )
+        .sort((a, b) => {
+          if (parseInt(a.rank) > parseInt(b.rank)) {
+            return this.sortOrder;
+          }
+
+          return altOrder;
+        });
+    },
+  },
+
   methods: {
     goToCoin(id) {
       this.$router.push({ name: "coin-detail", params: { id } });
+    },
+
+    changeSortOrder() {
+      this.sortOrder = this.sortOrder == 1 ? -1 : 1;
     },
   },
 };
